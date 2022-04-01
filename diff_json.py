7b79f58ac568f67c7f24796b94afd82c4bb2e9ff
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2022-04-01 10:21:21 -0400

    document the jq alternatives to diff_json.py

--- diff_json.py
+++ diff_json.py
@@ -9,3 +9,10 @@ if data1 == data2:
     print("Files JSON-identical")
 else:
     print("FILES non JSON-identical")
+
+# Alternative commands using jq, recommended by Samuel Larkin:
+# sha1sum  <(jq --sort-keys . file1.json) <(jq --sort-keys . file2.json)
+# diff <(jq --sort-keys . file1.json) <(jq --sort-keys . file2.json)
+# vimdiff <(jq --sort-keys . file1.json) <(jq --sort-keys . file2.json)
+# or, maybe you just want to compare the "source" elements:
+# vimdiff <(jq --sort-keys .source file1.json)  <(jq --sort-keys .source file2.json)
