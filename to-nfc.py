5ba5c6aee521d49fd07b5d1148284ec407566130
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2022-06-13 16:30:12 -0400

    to-nf[cd]: support stdin input

--- to-nfc.py
+++ to-nfc.py
@@ -4,7 +4,12 @@ import unicodedata as ud
 import sys
 sys.stdout.reconfigure(encoding="utf8")
 
-infile = sys.argv[1]
-with open(infile, encoding="utf8") as f:
-    for line in f:
+if len(sys.argv) > 1:
+    infile = sys.argv[1]
+    with open(infile, encoding="utf8") as f:
+        for line in f:
+            print(ud.normalize("NFC", line), end="")
+else:
+    sys.stdin.reconfigure(encoding="utf8")
+    for line in sys.stdin:
         print(ud.normalize("NFC", line), end="")
