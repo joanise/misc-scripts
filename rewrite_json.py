e78e9e400d73080f9f65ca95a599220d9eeac9a4
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2023-10-18 10:59:57 -0400

    fix: rewrite_json needs to set utf8 on stdout, on Windows

--- rewrite_json.py
+++ rewrite_json.py
@@ -3,11 +3,17 @@
 # Usage: ./rewrite_json.py g2p-mapping.json > compact-g2p-mapping.json
 # Requirements: pip install g2p
 
+import io
 import json
 import sys
 
 from g2p.mappings.utils import CompactJSONMappingEncoder
 
+# We always want utf-8, darn it, even when Windows doesn't think so.
+if sys.stdout.encoding != "utf8" and hasattr(sys.stdout, "buffer") and "pytest" not in sys.modules:
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf8")
+
 with open(sys.argv[1], encoding="utf8") as f:
     data = json.load(f)
+
 json.dump(data, sys.stdout, indent=4, ensure_ascii=False, cls=CompactJSONMappingEncoder)
