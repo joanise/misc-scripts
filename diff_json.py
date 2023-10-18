691c113dfccfc5edc8aa995957a0b785578154ad
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2023-10-18 10:34:18 -0400

    feat: diff_json.py can now detect and report same contents but in different order

--- diff_json.py
+++ diff_json.py
@@ -1,12 +1,20 @@
 import json
 import sys
 
+def same_modulo_order(data1, data2):
+    try:
+        return ordered(data1, key=json.dumps) == ordered(data2, key=json.dumps)
+    except:
+        return False
+
 with open(sys.argv[1], encoding="utf8") as f:
     data1 = json.load(f)
 with open(sys.argv[2], encoding="utf8") as f:
     data2 = json.load(f)
 if data1 == data2:
     print("Files JSON-identical")
+elif same_modulo_order(data1, data2):
+    print("Files have the same contents in a different order.")
 else:
     print("FILES non JSON-identical")
 
